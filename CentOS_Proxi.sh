44a5a612d747a84ba6e5e5ea20e06f77091c3206
Furkan Kamiloğlu <furkank6116@hotmail.com>
2022-04-18 23:18:41 +0300

    Update CentOS_Proxi.sh

--- CentOS_Proxi.sh
+++ CentOS_Proxi.sh
@@ -201,7 +201,7 @@ EOF
 
 bash /etc/rc.local
 
-squid_yukle && socks5_yukle && proxy_txt && jq_yukle && file_io_yukle
+squid_yukle && proxy_txt && jq_yukle && file_io_yukle
 
 echo -e "\n$sari IPv4   Proxy »$yesil ${IP4}:${IPV4_PORT}:${KULLANICI}:${SIFRE}$renkreset"
 echo -e "$sari SOCKS5 Proxy »$yesil ${IP4}:${SOCKS5_PORT}:${KULLANICI}:${SIFRE}$renkreset\n"
